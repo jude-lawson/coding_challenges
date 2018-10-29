abd8ec466d3380bff95a3336f8eb9852b663c44d
Jude Lawson <lawsonjdutton@gmail.com>
2018-10-29 15:28:26 -0600

    Adds in another round/refactor of FizzBuzz in JavaScript

--- javascript_challenges/fizz_buzz.js
+++ javascript_challenges/fizz_buzz.js
@@ -5,25 +5,25 @@ const rl = readline.createInterface({
   output: process.stdout
 });
 
-rl.question('What range do you want? ', range => {
-  var range = Array.from({length: (parseInt(range) + 1)}, (_initial_value, index) => index)
-  range.shift();
-
-  fizzBuzzIt(range);
+rl.question('What is the range that you want? ', answer => {
+  fizzBuzzIt(rangeFrom(answer));
 
   rl.close();
 });
 
+const rangeFrom = upperLimit => {
+  let range = Array.from({length: (parseInt(upperLimit) + 1)}, (_initialValue, index) => index);
+  range.shift();
+
+  return range;
+}
+
+
 const fizzBuzzIt = range => {
   range.forEach(number => {
-    if ((number % 3 === 0) && (number % 5 === 0)) {
-      console.log('FizzBuzz');
-    } else if (number % 3 === 0) {
-      console.log('Fizz')
-    } else if (number % 5 === 0) {
-      console.log('Buzz');
-    } else {
-      console.log(number);
-    }
+    if (number % 3 === 0 && number % 5 === 0) console.log('FizzBuzz')
+    else if (number % 3 === 0) console.log('Fizz')
+    else if (number % 5 === 0) console.log('Buzz')
+    else console.log(number)
   });
 }
